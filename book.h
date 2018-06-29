6c9c851c45dcd03cb8dc822e0c82b3082b495afe
Nathan Fox <fox016@gmail.com>
2018-06-29 16:16:08 -0600

    Create book.h

---
+++ book.h
@@ -0,0 +1,28 @@
+#ifndef BOOK_H_
+#define BOOK_H_
+
+#include <string>
+
+using namespace std;
+
+class Book
+{
+public:
+
+  Book();
+
+  ~Book();
+
+  Book(string title);
+
+  string getTitle();
+
+  void setTitle(string title);
+
+private:
+
+  string title;
+
+};
+
+#endif /* BOOK_H_ */
